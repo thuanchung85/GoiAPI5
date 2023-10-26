5987aec35e6d0d36a3dbd63d398345a7e6cd44ff
Thuận Chung Lương <thuanchung85@gmail.com>
2023-10-26 09:25:57 +0700

    save

--- Sources/GoiAPI5/GoiAPI5.swift
+++ Sources/GoiAPI5/GoiAPI5.swift
@@ -1,6 +1,33 @@
-public struct GoiAPI5 {
-    public private(set) var text = "Hello, World!"
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import Foundation
+import SwiftUI
+import UniformTypeIdentifiers
+
+public struct RecoveryWalletView: View {
+   
+  
+    
     public init() {
+        
     }
-}
+    
+    public var body: some View{
+        NavigationView {
+            //Choose View
+            VStack(alignment: .center) {
+                Text("GOI API 5")
+               
+            }
+            .padding(.bottom,50)
+            
+        }
+        //
+    }
+    
+    
+    
+    
+}//end struct
+
